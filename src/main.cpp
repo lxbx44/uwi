e35bfdee0011b26dddd931d955db276b08013589
Ariarch <rogerfiblasalvado@gmail.com>
2023-11-07 09:00:01 +0100

    add --tokens/-T

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,10 @@
 #include "./parser.hpp"
 #include "./tokenViewer.hpp"
 
-#define UWIC_VERSION "alpha 1.1"
+#define UWIC_VERSION "0.1.1 BETA"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Incorrect usage of uwic" << std::endl;
-        std::cerr << "uwic <file.uwi>" << std::endl;
-        return EXIT_FAILURE;
-    } else if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
+    if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
         std::cout << "uwic version:" << std::endl;
         std::cout << UWIC_VERSION << std::endl;
         return EXIT_SUCCESS;
@@ -30,21 +26,65 @@ int main(int argc, char* argv[]) {
         std::cout << "UWIC, the official compiler\nfor the uwi programming language\n" << std::endl;
         std::cout << "Usage: `uwic <file.uwi>`" << std::endl;
         std::cout << "Options:" << std::endl;
-        std::cout << "    -h           show this help page" << std::endl;
-        std::cout << "    --help       show this help page" << std::endl;
-        std::cout << "    -v           show version of the program" << std::endl;
-        std::cout << "    --version    show version of the program" << std::endl;
+        std::cout << "    -h              show this help page" << std::endl;
+        std::cout << "    --help          show this help page" << std::endl;
+        std::cout << "    -v              show version of the program" << std::endl;
+        std::cout << "    --version       show version of the program" << std::endl;
+        std::cout << "    -T / --tokens    view your uwi code as tokens" << std::endl;
+
         std::cout << "\nIf you notice any bugs open an isue in the github repo:"  << std::endl;
         std::cout << "<https://github.com/lxbx44/uwi>" << std::endl;
         return EXIT_SUCCESS;
-    }
 
+
+
+    } else if (strcmp(argv[1], "-T") == 0 || strcmp(argv[1], "--tokens") == 0) {
+        std::string arg = argv[2];
+
+        if (arg.find('.') == std::string::npos) {
+            std::cerr << "Incorrect usage of uwic" << std::endl;
+            std::cerr << "uwic <arrguments> <file.uwi>" << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        std::stringstream ss(arg);
+        std::string t;
+
+        std::vector<std::string> ts;
+
+        while (std::getline(ss, t, '.')) {
+            ts.push_back(t);
+        }
+
+        std::string uwiname = ts[0];
+
+        std::string contents;
+        {
+            std::stringstream contents_stram;
+            std::fstream input(argv[2], std::ios::in);
+            contents_stram << input.rdbuf();
+            contents = contents_stram.str();
+        }
+
+        std::vector<Token> tokens = tokenize(contents, uwiname);
+
+        viewTokens(tokens);
+
+        return EXIT_SUCCESS;
+    } 
+
+    if (argc != 2) {
+        std::cerr << "Incorrect usage of uwic" << std::endl;
+        std::cerr << "uwic <arrguments> <file.uwi>" << std::endl;
+        return EXIT_FAILURE;
+
+    }
 
     std::string arg = argv[1];
 
     if (arg.find('.') == std::string::npos) {
         std::cerr << "Incorrect usage of uwic" << std::endl;
-        std::cerr << "uwic <file.uwi>" << std::endl;
+        std::cerr << "uwic <arrguments> <file.uwi>" << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -71,7 +111,7 @@ int main(int argc, char* argv[]) {
 
     std::vector<Token> parsedTokens = tokenParser(tokens);
     
-    viewTokens(parsedTokens);
+    //viewTokens(parsedTokens);
 
     /*
     {
